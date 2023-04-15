67abe7dfcdcf9f2e350cf32ab5306b0683d4f71b
Tiago de Oliveira <deoliv.tiago@proton.me>
2023-04-15 01:25:25 -0300

    chore: add code analyzer

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule ClarxApi.MixProject do
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [ci: :test, "test.reset": :test],
       aliases: aliases(),
       deps: deps()
     ]
@@ -42,7 +43,8 @@ defmodule ClarxApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.22"},
       {:jason, "~> 1.4"},
-      {:plug_cowboy, "~> 2.6"}
+      {:plug_cowboy, "~> 2.6"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -57,7 +59,15 @@ defmodule ClarxApi.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "test.reset": ["ecto.drop", "test"],
+      ci: [
+        "deps.unlock --check-unused",
+        "compile --warnings-as-errors",
+        "format --check-formatted",
+        "credo --strict",
+        "test"
+      ]
     ]
   end
 end
